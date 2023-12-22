8ea395bb7ba187ceb73fbcc69288e2b32ddb5249
Md Kamrul Islam <kamruldca@gmail.com>
2023-12-22 18:31:52 +0600

    profile picture and name show

--- src/DashBoard/Profile/Profile.jsx
+++ src/DashBoard/Profile/Profile.jsx
@@ -1,10 +1,16 @@
-
+import React, { useContext } from 'react';
+import { AuthContext } from '../../AuthenticationPage/AuthProvider/AuthProvider';
 
 const Profile = () => {
+    const { user } = useContext(AuthContext);
+
+    console.log(user)
+
     return (
         <div className="items-center justify-center flex mx-auto flex-col">
-            <img className="md:w-96" src="https://i.ibb.co/rZs5GcS/myPhoto.png" alt="" />
-            <h1 className="mt-6 text-5xl">Profile Picture </h1>
+            <img className="md:w-96" src={user?.photoURL} alt="Profile" />
+            <h1 className="mt-6 text-3xl md:text-5xl font-extrabold">{user?.displayName
+}</h1>
         </div>
     );
 };
